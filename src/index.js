3f13c54c06a14cf5e4da0fc3501cbf7c2dfe5f5a
gpitrella <gabriel98_@hotmail.com>
2023-06-06 15:18:55 -0300

    update react 17

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "react-alice-carousel/lib/alice-carousel.css";
 import CryptoContext from "./CryptoContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById("root");
+render(
   <React.StrictMode>
     <CryptoContext>
       <App />
     </CryptoContext>
   </React.StrictMode>
-);
+, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
