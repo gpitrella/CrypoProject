b5315b9a61c441e50747fab3a6a9edd15aaf201b
gpitrella <gabriel98_@hotmail.com>
2023-06-06 15:27:23 -0300

    update react 17-parser

--- src/Pages/CoinPage/CoinPage.jsx
+++ src/Pages/CoinPage/CoinPage.jsx
@@ -105,7 +105,7 @@ const CoinPage = () => {
           {coin?.name}
         </Typography>
         <Typography variant="subtitle1" className={classes.description}>
-          {ReactHtmlParser(coin?.description.en.split(". ")[0])}.
+          {coin?.description.en.split(". ")[0]}.
         </Typography>
         <div className={classes.marketData}>
           <span style={{ display: "flex" }}>
